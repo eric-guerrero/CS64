1700e197cf39f925bbf7bf3281e79cb7c77bc001
Eric Guerrero <52993762+eric-guerrero@users.noreply.github.com>
2019-10-20 15:27:00 -0700

    Update arithmetic.asm

--- Lab3/arithmetic.asm
+++ Lab3/arithmetic.asm
@@ -1,47 +1,59 @@
-# hello.asm
-# A "Hello World" program in MIPS Assembly for CS64
+# Arithmetic.asm
+# A simple calculator program in MIPS Assembly for CS64
 #
-#  Data Area - allocate and initialize variables
-.data
-	# TODO: Write your string definitions here
-intro:	.asciiz	"Choose an integer number betwen 0 and 1000:\n"
-hell: .asciiz "Hello World!\n"
-usr: .asciiz "User chose "
-fin: .asciiz ". Truly a wise choice.\n"
-#Text Area (i.e. instructions)
+
 .text
 main:
-	#Print intoduction
-	li $v0,4	#to print a string
-	la $a0, intro 	#load string into a{0}
-	syscall		#print to usr
-	
-	#Get usr number
-	li $v0,5
-	syscall
-	move $t0, $v0	#temp{0} holds usr input
-	
-	#Print HelloWorld
-	li $v0,4
-	la $a0, hell
-	syscall
-
-	#Print before usr input
-	li $v0,4
-	la $a0, usr
-	syscall
-
-	#Print usr input
-	li $v0,1
-	move $a0, $t0
-	syscall
-	
-	#Print rest of statement
-	li $v0, 4
-	la $a0,fin
-	syscall
 
 	# TODO: Write your code here
+	
+	#Get arg 1 from usr and place it into a0
+	li $v0,5
+	syscall
+	move $a0, $v0
+	
+	#move arg 1 to temp{0}
+	move $t0,$a0
+	
+	#Get arg 2 from usr and place it into a0
+	li $v0,5
+	syscall
+	move $a0, $v0
+
+	#move arg 2 to temp{1}
+	move $t1,$a0
+
+	#Get arg 3 from usr and place it into a0
+	li $v0,5
+	syscall
+	move $a0,$v0
+
+	#move arg 3 to temp {4}
+	move $t4,$a0
+	
+	#(a-b) = d
+	sub $t3,$t0,$t1
+
+	# 2(d) == d + d
+	add $t3,$t3,$t3
+	
+	#3(c) = $t5
+	li $s0,3
+	mult $t4,$s0
+	mflo $t5
+
+
+	# (d + d) + 3*c
+	add $t7,$t3,$t5
+	move $a0,$t7
+
+	#Print what's in a{0}
+	li $v0,1
+	#move $a0,$t0
+	syscall
+
+
+	
 
 exit:
 	# Exit
